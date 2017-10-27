ce2dc8a632daa5a099ddf1410f9fdd8772cff4b2
Jonathan Spalink <spalink@gmail.com>
2017-10-27 16:36:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="readability-lxml",
-    version="0.2.6.1",
+    name="readability",
+    version="999999,
     author="Yuri Baburov",
     author_email="burchik@gmail.com",
     description="fast python port of arc90's readability tool",
